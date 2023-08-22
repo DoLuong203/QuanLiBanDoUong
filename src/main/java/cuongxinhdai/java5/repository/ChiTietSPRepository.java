3926332ffd66834e0f1c5f4510ee22d632469485
cuongdanglamgiau <cuongbcph27577@fpt.edu.vn>
2023-08-22 23:28:45 +0700

    update ngày 22/8

--- src/main/java/cuongxinhdai/java5/repository/ChiTietSPRepository.java
+++ src/main/java/cuongxinhdai/java5/repository/ChiTietSPRepository.java
@@ -3,7 +3,9 @@ package cuongxinhdai.java5.repository;
 import cuongxinhdai.java5.model.ChiTietSP;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChiTietSPRepository  extends JpaRepository<ChiTietSP, UUID> {
+   
 }
